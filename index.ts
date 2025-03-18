db5e496d73405cd26a6cb5f2b8061742614dd0fc
KendallRey <107897863+KendallRey@users.noreply.github.com>
2025-03-18 12:43:27 +0800

    added initial component template

--- index.ts
+++ index.ts
@@ -1,8 +1,27 @@
 import * as fs from 'fs'
 import * as path from 'path'
 
+import Mustache from 'mustache'
+
+const componentTemplate = fs.readFileSync(
+  path.resolve(__dirname, './templates/component.mustache'),
+  'utf-8'
+)
+
+const COMPONENTS = ["Autocomplete", "Button"]
+
 const componentsDir = path.join(__dirname, 'components');
 
 if (!fs.existsSync(componentsDir)) {
   fs.mkdirSync(componentsDir, { recursive: true })
 }
+
+COMPONENTS.forEach((component) => {
+  const componentPath = path.join(componentsDir, component)
+  fs.mkdirSync(componentPath, { recursive: true })
+  const content = Mustache.render(componentTemplate, {
+    prefix: 'Ken',
+    name: component,
+  })
+  fs.writeFileSync(path.join(componentPath, `${component}.tsx`), content)
+})
